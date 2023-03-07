003c19f0c9336b3fc90d53236ab92c4759b469a1
= <=>
2023-03-07 22:39:40 +0100

    Finished the adaptation for smaller devices (less than 350px) and made a scrolling to needed sections in header's menu.

--- script/script.js
+++ script/script.js
@@ -10,3 +10,19 @@ $(window).scroll(function(){
 $('#top').on('click', function(){
     $('html, body').animate({scrollTop: 0}, 700)
 })
+
+// Scroll
+$('#about').on('click', function(e){
+    e.preventDefault();
+    $('html, body').animate({scrollTop: $('#about-me').offset().top}, 500)
+})
+
+$('#skills').on('click', function(e){
+    e.preventDefault();
+    $('html, body').animate({scrollTop: $('#skills-section').offset().top}, 500)
+})
+
+$('#contact').on('click', function(e){
+    e.preventDefault();
+    $('html, body').animate({scrollTop: $('#form-section').offset().top}, 500)
+})
