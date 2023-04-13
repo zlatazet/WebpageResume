6a78272af417d88278e8e69d0c024b106bc93f8c
= <=>
2023-04-13 23:32:24 +0200

    coding-project page was completely changed (variant design number 2)

--- Coding-projects/js/script.js
+++ Coding-projects/js/script.js
@@ -1,31 +1,3 @@
-let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  if (n > slides.length) {slideIndex = 1} 
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none"; 
-  }
-  if (window.innerWidth < 981) {
-    slides[slideIndex-1].style.display = "block";
-  } else {
-    slides[slideIndex-1].style.display = "flex";
-  }
-}
-
 // Scroll
 $('#home').on('click', function(e){
   e.preventDefault();
