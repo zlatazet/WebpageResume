179d2c7f2f1e49ffa8d9314cead5295e295052b5
= <=>
2023-08-23 19:40:13 +0200

    changed burger menu opener

--- Coding-projects/js/script.js
+++ Coding-projects/js/script.js
@@ -18,3 +18,21 @@ $('#contact').on('click', function(e){
   e.preventDefault();
   $('html, body').animate({scrollTop: $('../index.html#form-section').offset().top}, 500)
 })
+
+// Menu opener
+const menu = document.getElementById("menu");
+const listContainer = document.getElementsByClassName("nav__items-con")[0];
+const body = document.querySelector("body");
+let clickCount = 0;
+
+menu.onclick = function() {
+    clickCount++;
+    menu.classList.toggle("openmenu");
+    if (clickCount % 2 === 0) {
+        listContainer.style.display = "none";
+        body.style.overflow = "scroll";
+    } else {
+        listContainer.style.display = "block";
+        body.style.overflow = "hidden";
+    }
+};
--- script/script.js
+++ script/script.js
@@ -1,3 +1,21 @@
+// Menu opener
+const menu = document.getElementById("menu");
+const listContainer = document.getElementsByClassName("nav__items-con")[0];
+const body = document.querySelector("body");
+let clickCount = 0;
+
+menu.onclick = function() {
+    clickCount++;
+    menu.classList.toggle("openmenu");
+    if (clickCount % 2 === 0) {
+        listContainer.style.display = "none";
+        body.style.overflow = "scroll";
+    } else {
+        listContainer.style.display = "block";
+        body.style.overflow = "hidden";
+    }
+};
+
 // Button that bring you to the top of the page
 $(window).scroll(function(){
     if($(this).scrollTop() > 500) {
